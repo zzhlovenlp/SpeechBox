9347e63f98560d3c6bc84d94b6aadbf5bc86e3a1
Aleksi Vesanto <avjves@utu.fi>
2018-03-06 13:29:31 +0000

    Clean file structure

--- network/keygen.sh
+++
@@ -1,6 +0,0 @@
-mkdir keys
-openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048
-openssl rsa -in private.pem -outform PEM -pubout -out ./keys/public.pem
-openssl rsa -in private.pem -out ./keys/private_unencrypted.pem -outform PEM
-
-
